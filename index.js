d5e5eee34a53f74243d398586147c44e3a6bb75b
Ibiza-rey <ibiza.reymundis@gmail.com>
2022-03-07 14:09:21 -0500

    Encriptador funciona por consola

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-function encriptar(){
+let inp_letter = prompt("Ingrese una palabre");
+console.log(inp_letter);
 
-    let dato = document.querySelector("input");
+let cadena = inp_letter.split('');
+let cadena1 = [];
+for (i=0; i < cadena.length; i++){
+  
+  switch (cadena[i]){
+    case "a":
+    cadena1.push("ai");
+    break;
     
-    let arreglo = dato.value.split("");
-    console.log(arreglo);
-    let arreglo2 = dato.value.split("");
+    case "e":
+    cadena1.push("enter");
+    break;
     
-         
-      for(let i =0; i < arreglo.length; i++){
-      
-      if (arreglo[i] == "a"){
-        arreglo2[i] = "ai";}
-        
-      if (arreglo[i] == "e"){
-        arreglo2[i] = "enter";}
-        
-      if (arreglo[i] == "i"){
-        arreglo2[i] = "imes";}
-        
-      if (arreglo[i] == "o"){
-        arreglo2[i] = "ober";}
-      
-      if (arreglo[i] == "u"){
-        arreglo2[i] = "ufat";}
-    }
-    console.log(arreglo2);
-     
-    }
+    case "i":
+    cadena1.push("imes");
+    break;
     
-function encriptar();
+    case "o":
+    cadena1.push("ober");
+    break;
+    
+    case "u":
+    cadena1.push("ufat");
+    break;
+    
+    default:
+    cadena1.push(cadena[i]); 
+  }  
+  console.log(cadena1);
+}
