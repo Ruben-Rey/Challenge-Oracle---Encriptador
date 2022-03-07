e9598e70ba2ae7c269a142a487b83990089f5ca4
Ibiza-rey <ibiza.reymundis@gmail.com>
2022-03-07 16:09:11 -0500

    Encriptador front terminado con un pequeño error

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-let inp_letter = prompt("Ingrese una palabre");
-console.log(inp_letter);
 
-let cadena = inp_letter.split('');
-let cadena1 = [];
-for (i=0; i < cadena.length; i++){
+function encriptar () {
+
+  let inp_letter = document.getElementById('inp_text');
+  inp_letter = inp_letter.value;
+  let cadena = inp_letter.split('');
+  let cadena1 = [];
+  for (i=0; i < cadena.length; i++){
+    
+    switch (cadena[i]){
+      case "a":
+      cadena1.push("ai");
+      break;
+      
+      case "e":
+      cadena1.push("enter");
+      break;
+      
+      case "i":
+      cadena1.push("imes");
+      break;
+      
+      case "o":
+      cadena1.push("ober");
+      break;
+      
+      case "u":
+      cadena1.push("ufat");
+      break;
+      
+      default:
+      cadena1.push(cadena[i]); 
+    }  
+    console.log(cadena1.join(''));
+    let out_text = cadena1.join('');
+    document.getElementById('out_text').innerHTML= out_text;
+  }
   
-  switch (cadena[i]){
-    case "a":
-    cadena1.push("ai");
-    break;
-    
-    case "e":
-    cadena1.push("enter");
-    break;
-    
-    case "i":
-    cadena1.push("imes");
-    break;
-    
-    case "o":
-    cadena1.push("ober");
-    break;
-    
-    case "u":
-    cadena1.push("ufat");
-    break;
-    
-    default:
-    cadena1.push(cadena[i]); 
-  }  
-  console.log(cadena1);
-}
+  }
