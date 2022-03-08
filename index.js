4846126c5e273f9747a987824fcacb13d7372407
Ibiza-rey <ibiza.reymundis@gmail.com>
2022-03-07 23:09:46 -0500

    Se enlazo el encriptador y desencritador

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function encriptar () {
 
-  let inp_letter = document.getElementById('inp_text');
+  let inp_letter = document.getElementById('encrip__inp-text');
   inp_letter = inp_letter.value;
   let cadena = inp_letter.split('');
   let cadena1 = [];
@@ -33,7 +33,24 @@ function encriptar () {
     }  
     console.log(cadena1.join(''));
     let out_text = cadena1.join('');
-    document.getElementById('out_text').innerHTML= out_text;
+    document.getElementById('encrip__out-text').innerHTML= out_text;
   }
   
   }
+
+function desencriptar() {
+
+  let frase = document.getElementById("desencrip__inp_text");
+  frase = frase.value;
+    
+  frase1 = frase.replace(/ai/g, "a");
+  frase2 = frase1.replace(/enter/g, "e");
+  frase3 = frase2.replace(/imes/g, "i");
+  frase4 = frase3.replace(/ober/g, "o");
+  frase5 = frase4.replace(/ufat/g, "u");
+    
+  console.log(frase5);
+  document.getElementById("desencrip__out-text").innerHTML=frase5;
+    
+  }
+
